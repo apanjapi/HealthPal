704e2e3fcc4226757e270a4e5222eb6da85c916e
Lance Bulseco <lancebulseco@Lances-Air.hitronhub.home>
2021-10-05 19:39:27 -0700

    Navbar Component

---
+++ healthpal/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+import React from 'react';
+import Navbar from '../comp/navbar';
+
+
+
+export default function Home() {
+
+  return <Navbar>
+    
+  </Navbar>
+  
+}

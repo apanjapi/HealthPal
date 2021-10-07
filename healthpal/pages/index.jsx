2412112c6d2c11c348f8d81fa90be4cd4995d91c
Amelie Panja <peach.panja@gmail.com>
2021-10-06 23:18:32 -0600

    Fix styling for index.jsx and record.jxs

--- healthpal/pages/index.jsx
+++ healthpal/pages/index.jsx
@@ -1,19 +1,24 @@
 import styles from "../styles/Home.module.css";
 import Label from "../comp/Label";
+import TopBar from "../comp/Topbar";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h3>Tell us about yourself..</h3>
-      <div className={styles.inputbox}>
-        <Label value="Name" placeholder="Your Name" />
-        <Label value="Gender" placeholder="Your Gender" />
-        <Label value="Age" placeholder="Your Age" />
-        <Label value="Weight" placeholder="Weight(KG)" />
-        <Label value="Hight" placeholder="Hight(CM)" />
-        <h3>What is your Weight goal?</h3>
-        <Label value="Weight Goal" placeholder="Weight(KG)" />
+      <TopBar />
+      <div>
+        <h3 className={styles.heading}>Tell us about yourself</h3>
+        <div className={styles.inputbox}>
+          <Label value="Name" placeholder="Your Name" />
+          <Label value="Gender" placeholder="Your Gender" />
+          <Label value="Age" placeholder="Your Age" />
+          <Label value="Weight" placeholder="Weight(KG)" />
+          <Label value="Hight" placeholder="Hight(CM)" />
+          <h3 className={styles.heading}>What is your Weight goal?</h3>
+          <Label value="Weight Goal" placeholder="Weight(KG)" />
+        </div>
       </div>
+      <div>Bottom nav</div>
     </div>
   );
 }
---
+++ healthpal/pages/record.jsx
@@ -0,0 +1,31 @@
+import styles from '../styles/Home.module.css'
+import CaloriesInput from '../comp/CaloriesInput'
+import TopBar from "../comp/Topbar";
+//import Label from "../comp/Label";
+
+export default function Home() {
+  return (
+    <div className={styles.container2}>
+      <TopBar />
+        <h3 className={styles.heading}>Summary</h3>
+        <div>add component here</div>
+        <h3 className={styles.heading}>Breakfast</h3>
+        <div className={styles.container3}>
+            <CaloriesInput></CaloriesInput>
+            <CaloriesInput title="Liquid +"></CaloriesInput>
+        </div>
+        <h3 className={styles.heading}>Lunch</h3>
+        <div className={styles.container3}>
+            <CaloriesInput></CaloriesInput>
+            <CaloriesInput title="Liquid +"></CaloriesInput>
+        </div>
+        <h3 className={styles.heading}>Dinner</h3>
+        <div className={styles.container3}>
+            <CaloriesInput></CaloriesInput>
+            <CaloriesInput title="Liquid +"></CaloriesInput>
+        </div>
+        <div>Bottom nav</div>
+    </div>
+  
+  )
+}
